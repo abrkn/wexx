f92a3f7998635d14be6b2de3c46bf0484525af64
Andreas Brekken <andreas@brekken.com>
2017-03-27 13:01:04 +0700

    Export RetryClient

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ if (typeof regeneratorRuntime === 'undefined') {
 	require('babel-polyfill');
 }
 
+export const RetryClient = require('./Client/RetryClient').default;
 export const Client = require('./Client').default;
 export const Application = require('./Server/Application').default;
 export const Context = require('./Server/Context').default;
