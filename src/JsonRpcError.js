3cdb208168e68c1cdaeb1f9321e99941d3f1c440
Andreas Brekken <andreas@brekken.com>
2018-05-28 15:31:09 +0700

    Add fix for instanceof

--- src/JsonRpcError.js
+++ src/JsonRpcError.js
@@ -2,6 +2,9 @@ class JsonRpcError extends Error {
   constructor(message, options = {}) {
     super(message);
 
+    this.constructor = JsonRpcError;
+    this.__proto__ = JsonRpcError.prototype;
+
     const { code = -32000, data } = options;
 
     this.code = code;
